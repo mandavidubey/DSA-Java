9bd94ee7ccdf42355d713679b5bc2ebd6379cad2
Mandavi Dubey <mandavidubey2002@gmail.com>
2024-07-29 00:55:19 +0530

    Floyd's Triangle

---
+++ Pattern_Questions/Floyds_Triangle.java
@@ -0,0 +1,14 @@
+package Pattern_Questions;
+
+public class Floyds_Triangle {
+    public static void main(String[] args) {
+        int n = 5;
+        int count = 1;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(count++ + " ");
+            }
+            System.out.println();
+        }
+    }
+}

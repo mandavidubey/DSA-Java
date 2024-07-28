bb71413172555eb7325e37aff2854bb18759fab2
Mandavi Dubey <mandavidubey2002@gmail.com>
2024-07-28 22:27:34 +0530

    Solid Rectangle

---
+++ Pattern_Questions/01_Solid_Rectangle.java
@@ -0,0 +1,12 @@
+package Pattern_Questions;
+
+class Solid_Rectangle {
+    public static void main(String [] args){
+        for(int i = 1; i <= 4; i++){
+            for(int j = 1; j <= 5; j++){
+                System.out.print("*");
+            }
+        System.out.println();
+        }
+    }
+}

dd376f3da3edb39588219792570309f3f57f3bf2
Mandavi Dubey <mandavidubey2002@gmail.com>
2024-07-29 00:45:54 +0530

    Inverted Half Pyramid with Numbers

---
+++ Pattern_Questions/Inverted_Half_Pyramid_with_Numbers.java
@@ -0,0 +1,13 @@
+package Pattern_Questions;
+
+public class Inverted_Half_Pyramid_with_Numbers {
+    public static void main(String[] args) {
+        int n = 5;
+        for(int i = n; i >= 1; i--){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}

006aa5f51849553b21bc0231f0b2e2a2b2e69938
Mandavi Dubey <mandavidubey2002@gmail.com>
2024-07-29 00:49:13 +0530

    Inverted Half Pyramid with Numbers

--- Pattern_Questions/Inverted_Half_Pyramid_with_Numbers.java
+++ Pattern_Questions/Inverted_Half_Pyramid_with_Numbers.java
@@ -1,13 +1,25 @@
 package Pattern_Questions;
 
 public class Inverted_Half_Pyramid_with_Numbers {
-    public static void main(String[] args) {
-        int n = 5;
-        for(int i = n; i >= 1; i--){
-            for(int j = 1; j <= i; j++){
-                System.out.print(j + " ");
-            }
-            System.out.println();
+    // public static void main(String[] args) {
+    //     int n = 5;
+    //     for(int i = n; i >= 1; i--){
+    //         for(int j = 1; j <= i; j++){
+    //             System.out.print(j + " ");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+//Alternate Method
+public static void main(String[] args) {
+    int n = 5;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n-i+1; j++){
+            System.out.print(j + " ");
         }
+        System.out.println();
     }
 }
+}
+

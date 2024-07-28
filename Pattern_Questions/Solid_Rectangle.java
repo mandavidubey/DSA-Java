8304c35202a20faa81c84d1f7057960797263bfa
Mandavi Dubey <mandavidubey2002@gmail.com>
2024-07-28 22:34:05 +0530

    Solid Rectangle

--- Pattern_Questions/Solid_Rectangle.java
+++ Pattern_Questions/Solid_Rectangle.java
@@ -1,9 +1,11 @@
 package Pattern_Questions;
 
-class Solid_Rectangle {
+public class Solid_Rectangle {   
     public static void main(String [] args){
-        for(int i = 1; i <= 4; i++){
-            for(int j = 1; j <= 5; j++){
+        int m = 4;
+        int n = 5;
+        for(int i = 1; i <= m; i++){
+            for(int j = 1; j <= n; j++){
                 System.out.print("*");
             }
         System.out.println();
